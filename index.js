a562e94ad58d830b996184777f22e2f7f5e1d5ae
Jemaul Hunter <jemaulhunter@gmail.com>
2022-07-19 07:03:40 -0400

    commit tree 6e17e85626f1378e7df1145fb34438b2b81f4a79

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 62;
+let num2 = 1;
+let multiply = (num1 * num2);
 
-// Write your code here
+let random = Math.floor(Math.random() * 10);
+
+let num3 = 16;
+let num4 = 4;
+let mod = (num3 / num4);
+
+let max = Math.ceil(19.5);
